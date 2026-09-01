adbe2c2eb8817a468680c1bb93743c1989a9ae43
Helber Diaz <hd128274@student.wpsok.org>
2026-09-01 13:04:47 -0500

    turnid in hw3

---
+++ HW3.py
@@ -0,0 +1,33 @@
+#Name: Helber Diaz
+#Class: 5th Hour
+#Assignment: HW3
+
+
+#1. Print "Hello World!"
+
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+Int = 3
+Stren = "45"
+Bool = True
+
+#3. Print all three variables on the same print function (at the same time).
+print(Int)
+print(Stren)
+print(Bool)
+#4. Create a variable that asks the user to input an integer.
+Input=int(input("enter 23 "))
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+tor=Int+Input
+print(tor)
+#6. Take the result from #5 and divide it by 2. Print the result.
+print(tor/2)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+Bool=False
+#8. Print the value of the boolean variable.
+print(Bool)
+#9. Create a variable with a number that contains decimals.
+Varbo=4.2
+
+#10. Round the number from #9 up or down using the round function.
+round(Varbo)
+
